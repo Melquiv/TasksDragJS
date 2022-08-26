f64cca10e73d30e8a00d867f16eea5d56e0c9904
melqui1998 <melque2012@gmail.com>
2022-08-26 13:10:18 -0300

    Ajustes Finais e Estilização

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 document.getElementById("adicionar").addEventListener("click", createTask);
 
+
+window.onload = function(){
+    document.querySelector("#dia").innerHTML = getWeekAndDate();
+};
+
 //Sortable.js
 const Dragarea = document.getElementsByClassName("container")[0];
 new Sortable(Dragarea , {
@@ -9,7 +14,13 @@ animation: 300
 
 
 //Retorna data + hora atual no formato: "DD/MM/YYY <br> HH:HH"
-function getDate(){
+function getHour(){
+    const data = new Date();
+
+    return data.getHours() + ':' + String(data.getMinutes()).padStart(2, "0");
+}
+
+function getWeekAndDate(){
     const data = new Date();
     const yyyy = data.getFullYear();
     let mm = data.getMonth() + 1; // Months start at 0!
@@ -20,9 +31,10 @@ function getDate(){
 
     var agora = dd + '/' + mm + '/' + yyyy;
 
-    return agora + '<br>' + data.getHours() + ':' + String(data.getMinutes()).padStart(2, "0");
-}
+    const semana = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
 
+    return semana[data.getDay()] + ', ' + agora;
+}
 
 function getTaskText(){
     return document.querySelector("#taskContent").value;
@@ -33,82 +45,77 @@ function cleanTaskText(){
     document.querySelector("#taskContent").focus();
 }
 
-
-
 function createTask(){
-    let task = document.createElement('div');
-    task.className = 'task-row';
-    task.style.display = 'flex';
+    if(getTaskText() == ''){
+        alert('Digite uma Tarefa');
+    }else{
 
-    
-    let taskDate = document.createElement('div');
-    taskDate.className = 'date content';
-    
+        let container = document.querySelector('.container');
 
-    let taskDateDiv = document.createElement('div');
-    taskDateDiv.innerHTML = getDate();
-    
-
-    
-    let btn2 = document.createElement('a')
-    let ico2 = document.createElement('i');
-    ico2.className = 'bi bi-square'; 
-    btn2.appendChild(ico2);
-
-
-
-    taskDate.appendChild(taskDateDiv);
-    taskDate.appendChild(btn2);
-
-
-
-    let taskContent = document.createElement('div');
-    taskContent.className = 'task content';    
-    taskContent.innerHTML = getTaskText();
-
-    let taskButtons = document.createElement('div');
-    taskButtons.className = 'buttons content';
-
-        let btn1 = document.createElement('a')
-        let ico1 = document.createElement('i');
-        ico1.className = 'bi bi-trash'; 
-        btn1.appendChild(ico1);
-
-
+        let task = document.createElement('div');
+        task.className = 'task-row';
+        task.style.display = 'flex';
         
+        let taskDate = document.createElement('div');
+        taskDate.className = 'date content';
 
-        let btn3 = document.createElement('a')
-        let ico3 = document.createElement('i');
-        ico3.className = 'bi bi-list'; 
-        btn3.appendChild(ico3);
+        let taskDateDiv = document.createElement('div');
+        taskDateDiv.innerHTML = getHour();
+        
+        let btn2 = document.createElement('a')
+        let ico2 = document.createElement('i');
+        ico2.className = 'bi bi-square'; 
+        btn2.appendChild(ico2);
+
+        taskDate.appendChild(taskDateDiv);
+        taskDate.appendChild(btn2);
+
+        let taskContent = document.createElement('div');
+        taskContent.className = 'task content';    
+        taskContent.innerHTML = getTaskText();
+
+        let taskButtons = document.createElement('div');
+        taskButtons.className = 'buttons content';
+
+            let btn1 = document.createElement('a')
+            let ico1 = document.createElement('i');
+            ico1.className = 'bi bi-trash'; 
+            btn1.appendChild(ico1);
+            
+            let btn3 = document.createElement('a')
+            let ico3 = document.createElement('i');
+            ico3.className = 'bi bi-list'; 
+            btn3.appendChild(ico3);
+        
+                taskButtons.appendChild(btn1);
+                taskButtons.appendChild(btn3);
+            
+        task.appendChild(taskDate);
+        task.appendChild(taskContent);
+        task.appendChild(taskButtons);
+
+
+
+        container.insertBefore(task, container.firstChild);
+
+        cleanTaskText();
+
+
+
+    //Função dos botões: Excluir, Feito
+        btn1.addEventListener('click', () => task.remove());
+
+        btn2.addEventListener('click', function(){
+            if(taskContent.className == 'task content'){
+                taskContent.className += " done";
+                ico2.className = "bi bi-check-square";
+                document.querySelector('.container').appendChild(task);
+            }else{
+                taskContent.className = 'task content';  
+                ico2.className = "bi bi-square";
+                container.insertBefore(task, container.firstChild);
+            }
+            });
     
-   
-            taskButtons.appendChild(btn1);
-            taskButtons.appendChild(btn3);
-
-        
-    task.appendChild(taskDate);
-    task.appendChild(taskContent);
-    task.appendChild(taskButtons);
-
-    document.querySelector('.container').appendChild(task);
-
-   cleanTaskText();
-
-
-
-   //Função dos botões: Excluir, Feito
-    btn1.addEventListener('click', () => task.remove());
-
-    // btn2.addEventListener('click', () => taskContent.className += " done");
-    btn2.addEventListener('click', function(){
-        if(taskContent.className == 'task content'){
-            taskContent.className += " done";
-            ico2.className = "bi bi-check-square";
-        }else{
-            taskContent.className = 'task content';  
-            ico2.className = "bi bi-square";
-        }
-        });
-        
+    }
 }
